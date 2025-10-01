b99d123f1fc75fe7efca660c0b6b4d8a4928d27d
Christopher Koski <ckoski18@gmail.com>
2025-10-01 14:47:21 -0400

    - Remove misplaced duplicate files

--- Halo-fi-IOS/Config.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum Config {
-    static let supabaseURL = "https://ucbqtodqepftjbltbezx.supabase.co"
-    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjYnF0b2RxZXBmdGpibHRiZXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTYyMDIsImV4cCI6MjA3MDQ5MjIwMn0.5-nDmWSr3Bqbdq-tykCUOjxrbv7W2KWPJBdRBgBUfM8"
-}
