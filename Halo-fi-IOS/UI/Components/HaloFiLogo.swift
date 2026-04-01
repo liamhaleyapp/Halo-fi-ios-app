4b77cfe1c8e16b9a5c76d4264dbb294c6817da88
Christopher Koski <ckoski18@gmail.com>
2026-04-01 16:57:59 -0400

    - Remaining image fix

--- Halo-fi-IOS/UI/Components/HaloFiLogo.swift
+++ Halo-fi-IOS/UI/Components/HaloFiLogo.swift
@@ -6,25 +6,16 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct HaloFiLogo: View {
     let size: CGFloat
 
-    private static let cachedImage: UIImage? = {
-        guard let url = Bundle.main.url(forResource: "halofi_logo", withExtension: "png"),
-              let data = try? Data(contentsOf: url) else { return nil }
-        return UIImage(data: data)
-    }()
-
     var body: some View {
-        if let uiImage = Self.cachedImage {
-            Image(uiImage: uiImage)
-                .resizable()
-                .scaledToFit()
-                .frame(width: size, height: size)
-                .accessibilityHidden(true)
-        }
+        Image("HaloFiLogo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: size, height: size)
+            .accessibilityHidden(true)
     }
 }
 
